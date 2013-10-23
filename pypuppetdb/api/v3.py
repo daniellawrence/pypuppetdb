9c5a77b5749d3b4e66b6ab9d08e3f0daf395dd1d
Daniel Lawrence <dannyla@linux.com>
2013-10-23 22:01:57 +1100

    DO NOT MERGE - Horrible hack for resource types
    Workaround until puppetdb fixes the resource type format.

--- pypuppetdb/api/v3.py
+++ pypuppetdb/api/v3.py
@@ -97,7 +97,7 @@ class API(BaseAPI):
             # Need to capitalize the resource type since PuppetDB doesn't
             # answer to lower case type names.
             # bugs.puppetlabs.com/some_value
-            type_ = type_.capitalize()
+            type_ = '::'.join(word[0].upper() + word[1:] for word in type_.split('::'))
             if title is not None:
                 path = '{0}/{1}'.format(type_, title)
             elif title is None:
